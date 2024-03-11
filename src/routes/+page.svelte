a88849e809e845262cec7734a9dc55b80ebce077
EggbertFluffle <hdiambrosio@gmail.com>
2024-03-11 18:41:09 -0400

    Fininshed floating letters

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,29 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+	import FloatingLetter from "./components/FloatingLetter.svelte";
+
+	let firstName: string = "Harrison";
+	let lastName: string = "DiAmbrosio";
+	let vowels: string = "aeiouAEIUO";
+</script>
+
+<h1>
+	{#each firstName as l}
+		<FloatingLetter letter={l} translationOffset={1500} rotationOffset={720} highlighted={vowels.includes(l)}/>
+	{/each}
+	<br/>
+	{#each lastName as l}
+		<FloatingLetter letter={l} translationOffset={1500} rotationOffset={720} highlighted={vowels.includes(l)}/>
+	{/each}
+</h1>
+
+<style>
+	:root {
+		background-color: #3c3836;
+	}
+
+	h1 {
+		padding: 0px;
+		font-size: 12em;
+		color: #ebdbb2;
+	}
+</style>
---
+++ src/routes/components/FloatingLetter.svelte
@@ -0,0 +1,36 @@
+<script lang="ts">
+	export let letter: string;
+	export let translationOffset: number;
+	export let rotationOffset: number;
+	export let highlighted: boolean;
+
+	let x: number = Math.floor(Math.random() * translationOffset) - (translationOffset / 2);
+	let y: number = Math.floor(Math.random() * translationOffset) - (translationOffset / 2);
+	let r: number = Math.floor(Math.random() * rotationOffset) - (rotationOffset / 2);
+</script>
+
+<p style="--x: {x}px; --y: {y}px; --r: {r}deg; --h: {highlighted ? '#fb4938' : 'inherit'};">{letter}</p>
+
+<style>
+	p {
+		position: relative;
+		font-size: inherit;
+		display: inline-block;
+		color: var(--h);
+		animation: float 2.5s cubic-bezier(0.25, 1, 0.5, 1);
+	}
+
+	@keyframes float {
+		from {
+			left: var(--x);
+			top: var(--y);
+			transform: rotate(var(--r));
+		}
+
+		to {
+			left: 0px;
+			top: 0px;
+			transform: rotate(0deg);
+		}
+	}
+</style>
